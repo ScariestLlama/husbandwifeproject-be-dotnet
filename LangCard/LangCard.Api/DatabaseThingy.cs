502be7a4b05b80af51d6fccbda807a3596c07367
Peter <peterhowelljones@gmail.com>
2023-10-29 18:13:42 +0000

    Db code extracted to service

---
+++ LangCard/LangCard.Api/DatabaseThingy.cs
@@ -0,0 +1,105 @@
+﻿using Google.Cloud.Datastore.V1;
+using Grpc.Core;
+using System.Reflection;
+
+namespace LangCard.Api
+{
+    public class DatabaseThingy
+    {
+        private readonly DatastoreDb _db;
+
+        public DatabaseThingy()
+        {
+            var envVars = Environment.GetEnvironmentVariables();
+
+            
+            var project = envVars["DATASTOREDB_PROJECT"].ToString();
+            
+            var devMode = envVars["DEVMODE"].ToString();
+
+            if(devMode.Equals("true", StringComparison.OrdinalIgnoreCase))
+            {
+                var host = envVars["DATASTOREDB_HOST"].ToString();
+                var port = Convert.ToInt32(envVars["DATASTOREDB_PORT"]);
+
+                var dsBuilder = new DatastoreClientBuilder();
+                dsBuilder.ChannelCredentials = ChannelCredentials.Insecure;
+                dsBuilder.Endpoint = $"{host}:{port}";
+                var client = dsBuilder.Build();
+
+                _db = DatastoreDb.Create(project, "", client);
+            }
+            else
+            {
+                _db = DatastoreDb.Create(project);
+            }
+
+            
+        }
+
+        public async Task<List<T>> Select<T>(string from) where T : class, new()
+        {
+            var things = new List<T>();
+            var results = await _db.RunQueryAsync(new Query(from));
+            foreach (var result in results.Entities)
+            {
+
+                if (result == null)
+                {
+                    continue;
+                }
+
+                var thing = new T();
+
+                Type myType = thing.GetType();
+                IList<PropertyInfo> props = new List<PropertyInfo>(myType.GetProperties());
+
+                foreach (PropertyInfo prop in props)
+                {
+                    if(prop.Name.Equals("id", StringComparison.OrdinalIgnoreCase))
+                    {
+                        prop.SetValue(thing, result.Key.Path.Last().Name);
+                        continue;
+                    }
+
+                    if(result.Properties.ContainsKey(prop.Name.ToLower()))
+                    {
+                        var dbProp = result.Properties[prop.Name.ToLower()];
+                        prop.SetValue(thing, dbProp.StringValue);
+                    }
+                }
+
+                things.Add(thing);
+            }
+
+            return things;
+        }
+
+        public async Task<string> Insert<T>(string into, T model) where T : class, new()
+        {
+            var entity = new Entity()
+            {
+                Key = _db.CreateKeyFactory(into).CreateKey(Guid.NewGuid().ToString())
+            };
+
+            Type myType = model.GetType();
+            IList<PropertyInfo> props = new List<PropertyInfo>(myType.GetProperties());
+
+            foreach (PropertyInfo prop in props)
+            {
+                if (prop.Name.Equals("id", StringComparison.OrdinalIgnoreCase))
+                {
+                    continue;
+                }
+
+                entity[prop.Name.ToLower()] = prop.GetValue(model) as string;
+            }
+
+            await _db.InsertAsync(entity);
+
+            return entity.Key.Path.Last().Name;
+        }
+    }
+
+   
+}
--- LangCard/LangCard.Api/Program.cs
+++ LangCard/LangCard.Api/Program.cs
@@ -1,8 +1,5 @@
-using Google.Cloud.Datastore.V1;
-using Grpc.Core;
-using Microsoft.AspNetCore.Http.HttpResults;
+using LangCard.Api;
 using Microsoft.AspNetCore.Mvc;
-using static Grpc.Core.Metadata;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,78 +17,19 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-var envVars = Environment.GetEnvironmentVariables();
-
-var host = envVars["DATASTOREDB_HOST"].ToString();
-var project = envVars["DATASTOREDB_PROJECT"].ToString();
-var port = Convert.ToInt32(envVars["DATASTOREDB_PORT"]);
-
-var dsBuilder = new DatastoreClientBuilder();
-dsBuilder.ChannelCredentials = ChannelCredentials.Insecure;
-dsBuilder.Endpoint = $"{host}:{port}";
-var client = dsBuilder.Build();
-
-var db = DatastoreDb.Create(project, "", client);
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+var db = new DatabaseThingy();
 
 app.MapGet("/cards", async () =>
 {
-    var cards = new List<CardRequest>();
-    var results = await db.RunQueryAsync(new Query("cards"));
-    foreach (var result in results.Entities)
-    {
-
-        if (result == null)
-        {
-            continue;
-        }
-
-        var card = new CardRequest();
-        card.Id = result.Key.Path.Last().Name;
-        foreach (var prop in result.Properties)
-        {
-            if(prop.Key.Equals("welsh", StringComparison.OrdinalIgnoreCase))
-            {
-                card.Welsh = prop.Value.StringValue;
-
-            }
-
-            if(prop.Key.Equals("english", StringComparison.OrdinalIgnoreCase))
-            {
-                card.English = prop.Value.StringValue;
-            }
-        }
-        cards.Add(card);
-    }
-
+    var cards = await db.Select<CardRequest>("cards");
     return cards;
 })
 .WithOpenApi();
 
 app.MapPost("/card", async ([FromBody] CardRequest model) =>
 {
-    var entity = new Entity()
-    {
-        Key = db.CreateKeyFactory("cards").CreateKey(Guid.NewGuid().ToString())
-    };
-
-    entity["welsh"] = model.Welsh;
-    entity["english"] = model.English;
-
-    var key = await db.InsertAsync(entity);
-
-    model.Id = entity.Key.Path.Last().Name;
-
+    model.Id = await db.Insert("cards", model);
     return model;
 });
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
