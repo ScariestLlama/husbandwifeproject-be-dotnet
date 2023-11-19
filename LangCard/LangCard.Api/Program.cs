12f643398856a1fd5096b2fdd1d6f234d49235d4
Peter <peterhowelljones@gmail.com>
2023-11-19 15:04:43 +0000

    adding cors

--- LangCard/LangCard.Api/Program.cs
+++ LangCard/LangCard.Api/Program.cs
@@ -17,7 +17,20 @@ if(cloudMode)
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+                   .AllowAnyHeader()
+                   .AllowAnyMethod();
+        });
+});
+
 var app = builder.Build();
+
+app.UseCors();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
